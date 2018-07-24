325ac3ff652fb13d16b5fc9898fbbe97d2d25ac6
XIECI <cisnoc.xie@oocl.com>
2018-07-24 14:54:16 +0800

    complete deleteEmployee

--- src/main/java/com/oocl/employee/controller/EmployeeController.java
+++ src/main/java/com/oocl/employee/controller/EmployeeController.java
@@ -3,10 +3,7 @@ package com.oocl.employee.controller;
 import com.oocl.employee.service.EmployeeService;
 import com.oocl.employee.bean.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,12 +18,17 @@ public class EmployeeController {
         return employeeService.getAllEmployees();
     }
 
-    @PostMapping("/employee")
+    @PostMapping("/employees")
     public Employee addEmployee(@RequestBody Employee employee) {
         Employee e = null;
         if (employeeService.addEmployee(employee)) {
             e = employee;
         }
         return e;
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public void delete(@PathVariable int id) {
+        employeeService.deleteEmployee(id);
     }
 }
