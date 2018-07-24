8dc3f676519143cdf3194714a5449eb261934a1a
XIECI <cisnoc.xie@oocl.com>
2018-07-24 17:28:48 +0800

    complete getEmployee

--- src/main/java/com/oocl/employee/controller/EmployeeController.java
+++ src/main/java/com/oocl/employee/controller/EmployeeController.java
@@ -19,16 +19,28 @@ public class EmployeeController {
     }
 
     @PostMapping("/employees")
-    public Employee addEmployee(@RequestBody Employee employee) {
+    public Employee postEmployee(@RequestBody Employee employee) {
         Employee e = null;
-        if (employeeService.addEmployee(employee)) {
+        if (employeeService.postEmployee(employee)) {
             e = employee;
         }
         return e;
     }
 
     @DeleteMapping("/employees/{id}")
-    public void delete(@PathVariable int id) {
+    public Void delete(@PathVariable int id) {
         employeeService.deleteEmployee(id);
+        return null;
+    }
+
+    @PutMapping("/employees/{id}")
+    public Employee putEmployee(@PathVariable int id, @RequestBody Employee employee) {
+        employeeService.putEmployee(id, employee);
+        return employee;
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id) {
+        return employeeService.getEmployee(id);
     }
 }
--- src/main/java/com/oocl/employee/service/EmployeeService.java
+++ src/main/java/com/oocl/employee/service/EmployeeService.java
@@ -20,7 +20,26 @@ public class EmployeeService {
         return employeeList;
     }
 
-    public boolean addEmployee(Employee employee) {
+    public boolean postEmployee(Employee employee) {
         return employeeList.add(employee);
+    }
+
+    public boolean deleteEmployee(int id) {
+        boolean isContain = false;
+        for (Employee employee : employeeList) {
+            if (employee.getId() == id) {
+                isContain = employeeList.remove(employee);
+            }
+        }
+        return isContain;
+    }
+
+    public Employee putEmployee(int id, Employee employee) {
+        employeeList.set(employeeList.indexOf(employeeList.stream().filter(employee1 -> employee1.getId() == id).findFirst().get()), employee);
+        return employee;
+    }
+
+    public Employee getEmployee(int id) {
+        return employeeList.stream().filter(employee -> employee.getId() == id).findFirst().get();
     }
 }
